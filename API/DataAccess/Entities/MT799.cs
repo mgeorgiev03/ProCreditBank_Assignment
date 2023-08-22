36119b4997b0c0a9db09de63c446d6c193a018c3
mgeorgiev03 <maxim.georgiev.2003@gmail.com>
2023-08-22 12:08:46 +0300

    DataAccess Layer
    No ORM, only database entity

---
+++ API/DataAccess/Entities/MT799.cs
@@ -0,0 +1,29 @@
+﻿namespace DataAccess.Entities
+{
+    public class MT799
+    {
+        public Guid Id { get; set; }
+
+        //for testing database, will be changed for the code below at a later stage
+        public string Message { get; set; }
+
+
+        ////MT799 Block 1
+        //public string SenderBIC { get; set; }
+        //public string SenderLTAddress { get; set; }
+
+        ////MT799 Block 2
+        //public string MessageType { get; set; } //is this needed, all messages will be mt799? 
+        //public string ReceiverBIC { get; set; }
+        //public DateOnly Date { get; set; }
+
+        ////MT 799 Block 4
+        //public string TransactionId { get; set; }
+        //public string AnotherId { get; set; } //??
+        //public string TextBlock { get; set; }
+
+        ////MT799 Block 5
+        //public string MAC { get; set; }
+        //public string CHK { get; set; }
+    }
+}
