982189d024646690c4498847496cc2c153acfb51
mgeorgiev03 <maxim.georgiev.2003@gmail.com>
2023-08-22 15:47:16 +0300

    Working prototype of Database connection
    CRUD operations working, need to change things when the model is updated. Tested on a separate console application

---
+++ API/DataAccess/DatabaseContext.cs
@@ -0,0 +1,29 @@
+﻿using DataAccess.Entities;
+using System.Data.SQLite;
+using System.Runtime.CompilerServices;
+
+namespace DataAccess
+{
+    public class DatabaseContext 
+    {
+        string connectionString = @"Data Source=APIDatabase.db;Version=3;"; 
+
+        public void CreateConnecton()
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "INSERT INTO MyTable (ColumnName) VALUES (@Value)";
+                using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Value", "SomeValue");
+                    command.ExecuteNonQuery();
+                }
+
+                connection.Close();
+            }
+
+        }
+    }
+}
